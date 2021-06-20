fba10fd611915a9ae04d25a01b8940e5bf769193
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 03:03:01 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -15,7 +15,7 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant runneradmin:F >nul
+ICACLS C:\Windows\installer /grant runneradmin:F >nul
 del /f "C:\Users\Administrator\Desktop\Microsoft Edge.lnk" > out.txt 2>&1
 ping -n 10 127.0.0.1 >nul
