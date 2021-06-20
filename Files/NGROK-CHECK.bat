25dec588c15e3d79b02d3aebd0615854a86df5f3
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 02:50:42 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,6 +1,5 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-del /f "C:\Users\Public\Desktop\Microsoft Edge.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator dagger#rdp01 /add >nul
@@ -18,4 +17,5 @@ sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+del /f "C:\Users\Administrator\Desktop\Microsoft Edge.lnk" > out.txt 2>&1
 ping -n 10 127.0.0.1 >nul
