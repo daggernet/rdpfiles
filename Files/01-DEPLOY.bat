8dae670c1bcf54a3f016777600c6e0c17cd5d71e
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 07:03:37 -0300

    Update 01-DEPLOY.bat

--- Files/01-DEPLOY.bat
+++ Files/01-DEPLOY.bat
@@ -1,5 +1,8 @@
 @echo off
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d d:\wallpaper.bmp /f 
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
 @echo Dagger RDP Deploy
+runas /noprofile /user:administrator D:\admin.bat
 del "%PUBLIC%\Desktop\Firefox.lnk"
 del "%PUBLIC%\Desktop\Microsoft Edge.lnk"
 del "%PUBLIC%\Desktop\Unity Hub.lnk"
@@ -9,12 +12,10 @@ echo.
 echo.
 @echo HitLeap: BUETFUXF8mEDoUErizJgZpiy5gZ3g54R
 echo.
-tzutil /s "Montevideo Standard Time"
 start /wait D:\notepad++.exe
 start /wait D:\winrar.exe /S
 start /wait D:\fastconfigvps
 start /wait D:\otohits.exe /S
 start /wait D:\hitleap.exe
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d d:\wallpaper.bmp /f 
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+
 pause 
