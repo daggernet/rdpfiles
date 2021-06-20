c9400d1024c78791195137a7612995d6cd88a37d
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 03:52:26 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+net config server /srvcomment:"DAGGER vps" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator dagger#rdp01 /add >nul
 net localgroup administrators administrator /add >nul
@@ -16,4 +16,7 @@ sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command Move-Item -Path C:\Users\Public\Downloads -Destination C:\Users\administrator\Downloads
+
 ping -n 10 127.0.0.1 >nul
