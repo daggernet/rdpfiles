e5eb74902df520284d380bde7c757ae1d515123e
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 06:44:41 -0300

    Update 01-DEPLOY.bat

--- Files/01-DEPLOY.bat
+++ Files/01-DEPLOY.bat
@@ -1,12 +1,18 @@
+@echo off
 @echo Dagger RDP Deploy
+del "%PUBLIC%\Desktop\Firefox.lnk"
+del "%PUBLIC%\Desktop\Microsoft Edge.lnk"
+del "%PUBLIC%\Desktop\Unity Hub.lnk"
+del "%PUBLIC%\Desktop\Google Cloud SDK Shell.lnk"
 echo.
 @echo Otohits: 3ad83f0a-78ee-422b-b2f2-4a1605540062
 echo.
 @echo HitLeap: BUETFUXF8mEDoUErizJgZpiy5gZ3g54R
 echo.
 tzutil /s "Montevideo Standard Time"
-start /wait D:\notepad++.exe /S
+start /wait D:\notepad++.exe
 start /wait D:\winrar.exe /S
+start /wait D:\fastconfigvps
 start /wait D:\otohits.exe /S
 start /wait D:\hitleap.exe
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d d:\wallpaper.bmp /f 
