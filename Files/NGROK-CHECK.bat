1cdb65f96e1047129293288089ad60edeae72ad7
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 02:58:05 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -2,12 +2,12 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator dagger#rdp01 /add >nul
-net localgroup administrators administrator /add >nul
+net user runneradmin dagger#rdp01 /add >nul
+net localgroup administrators runneradmin /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: administrator
+echo User: runneradmin
 echo Pass: dagger#rdp01
 curl -O https://raw.githubusercontent.com/daggernet/rdpfiles/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 
