24ef03e22c1790fd8a56b58bc2cc34dc85e836f4
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-20 05:33:41 -0300

    Update and rename wallpaper.bat to runasadmin.bat

--- Files/runasadmin.bat
+++ Files/runasadmin.bat
@@ -1,4 +1,6 @@
 @echo off
+start /wait notepad++.exe /S
+start /wait winrar.exe /S
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\wallpaper.jpg /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
